9daa7e57fb51e6b02cea095c5588a13714ea01dd
ajmalbinnizam <ajmalnizam369@gmail.com>
2021-05-22 23:30:54 +0530

    input output property

--- assignments/star/src/app/app.module.ts
+++ assignments/star/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';//import pipes
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- assignments/star/src/app/title-case.pipe.ts
+++ assignments/star/src/app/title-case.pipe.ts
@@ -1,3 +1,40 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'titleCase'
+// })
+// export class TitleCasePipe implements PipeTransform {
+
+//   transform(value: string): any {
+//     if (!value) return null;
+
+//     // let preposition = [ 
+//     //     'of', 'the','in'
+//     // ];
+//     let words = value.split(' ');
+//     for(var i = 0; i<words.length; i++){
+//       let word = words[i];
+//       if(i>0 && this.isPreposition(word))
+//       words[i] = words[i].toLowerCase();
+//       else{
+//         words[i] = this.toTitleCase(word);
+//       }
+//     }
+//     return words.join(' ');
+//   }
+
+// private isPreposition (word:string):boolean{
+//   let preposition = [ 
+//     'of', 'the','is', 'a','in', 'at'];
+// return preposition.includes(word.toLowerCase())
+// }
+// private toTitleCase(word: string): string {
+//   return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+// }
+
+
+// }
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,32 +42,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitleCasePipe implements PipeTransform {
 
+
   transform(value: string): any {
     if (!value) return null;
 
-    // let preposition = [ 
-    //     'of', 'the','in'
-    // ];
+    let preposition = ['the', 'is', 'a'];
     let words = value.split(' ');
     for(var i = 0; i<words.length; i++){
-      let word = words[i];
-      if(i>0 && this.isPreposition(word))
-      words[i] = words[i].toLowerCase();
+      if( preposition.includes(words[i].toLowerCase()))
+      words[i] = words[i].toLowerCase()
       else{
-        words[i] = this.toTitleCase(word);
+        words[i] = words[i].substr(0,1).toUpperCase() + words[i].substr(1).toLowerCase()
       }
     }
     return words.join(' ');
+
   }
 
-private isPreposition (word:string):boolean{
-  let preposition = [ 
-    'of', 'the','in', 'at'];
-return preposition.includes(word.toLowerCase())
-}
-private toTitleCase(word: string): string {
-  return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
-}
+
 
 
 }
